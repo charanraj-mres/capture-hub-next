fbd82c2405b62ffae5a97eff0af11080f439e9c2
charanraj-mres <charanraj@mresult.com>
2025-03-05 21:16:39 +0530

    dashboard added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
 import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/components/Auth/AuthProvider";
+import { SidebarProvider } from "@/context/SidebarContext";
+import { SecondThemeProvider } from "@/context/ThemeContext";
 import { Toaster } from "react-hot-toast";
 import ScrollToTop from "@/components/ScrollToTop";
 const font = Poppins({
@@ -18,19 +24,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${font.className}`}>
+      <body className={`${font.className} dark:bg-gray-900`}>
         <AuthProvider>
-          <ThemeProvider
-            attribute="class"
-            enableSystem={true}
-            defaultTheme="system"
-          >
-            <Toaster position="top-center" />
-            <Header />
-            {children}
-            <Footer />
-            <ScrollToTop />
-          </ThemeProvider>
+          <SecondThemeProvider>
+            <SidebarProvider>
+              <ThemeProvider
+                attribute="class"
+                enableSystem={true}
+                defaultTheme="system"
+              >
+                <Toaster position="top-center" />
+                <Header />
+                {children}
+                <Footer />
+                <ScrollToTop />
+              </ThemeProvider>
+            </SidebarProvider>
+          </SecondThemeProvider>
         </AuthProvider>
       </body>
     </html>
